8d9c55f6a5d56aa901753be534d1940503c35114
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-06-03 18:37:20 -0700

    Cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import SignInPage from "./components/signIn";
+import SignInPage from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
 import { useAuth } from "./contexts/AuthContext";
 
