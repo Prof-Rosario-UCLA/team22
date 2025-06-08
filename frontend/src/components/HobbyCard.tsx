0875058e8435c688eca47519216fb9ee76d6c9ab
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-06-07 19:36:22 -0700

    Made hobby cards prettier

--- frontend/src/components/HobbyCard.tsx
+++ frontend/src/components/HobbyCard.tsx
@@ -9,53 +9,46 @@ interface HobbyCardProps {
 
 const getDifficultyColor = (difficulty: string) => {
   switch (difficulty.toLowerCase()) {
-    case "easy":
+    case "beginner":
       return "text-green-600";
-    case "medium":
+    case "intermediate":
       return "text-yellow-500";
-    case "hard":
+    case "advanced":
       return "text-red-600";
     default:
       return "text-gray-500";
   }
 };
 
-const getProgressLabel = (progress: number): string => {
-  if (progress >= 100) return "Done";
-  if (progress >= 66) return "Tried It";
-  if (progress >= 33) return "Just Added";
-  if (progress == 0) return "Not Added";
-  return "Not started";
-};
-
-const HobbyCard: React.FC<HobbyCardProps> = ({ hobby, children, dragHandle }) => {
+const HobbyCard: React.FC<HobbyCardProps> = ({
+  hobby,
+  children,
+  dragHandle,
+}) => {
   const difficultyColor = getDifficultyColor(hobby.difficulty);
+  console.log(hobby.difficulty);
   return (
-    <article className="bg-white shadow-md rounded-lg p-4 border border-gray-200 mb-6 space-y-2">
-      <h3 className="text-xl font-bold text-gray-900">{hobby.name}</h3>
-        {dragHandle && (
-          <div className="mb-2">
-            <div
-              className="w-full h-5 bg-gray-300 rounded cursor-grab flex items-center justify-center text-xs font-medium text-gray-700 hover:bg-gray-400 transition"
-            >
-              {dragHandle}
-            </div>
+    <article className="bg-white shadow-md rounded-lg p-4 border border-stone-200 mb-6 space-y-2">
+      <h3 className="text-xl font-bold text-stone-800">{hobby.name}</h3>
+      {dragHandle && (
+        <div className="mb-2">
+          <div className="w-full h-5 bg-gray-300 rounded cursor-grab flex items-center justify-center text-xs font-medium text-gray-700 hover:bg-gray-400 transition">
+            {dragHandle}
           </div>
-        )}
+        </div>
+      )}
 
       {hobby.category && (
-        <p className="text-gray-700 text-sm">Category: {hobby.category}</p>
+        <p className="text-stone-500 text-sm">{hobby.category}</p>
       )}
 
       <p className={`text-sm font-medium ${difficultyColor}`}>
-        Difficulty: {hobby.difficulty || "N/A"}
+        {hobby.difficulty || "N/A"}
       </p>
 
-      {/* Progress Bar */}
-      <div className="text-sm text-gray-700">Progress: {hobby.progress}% - {getProgressLabel(hobby.progress)}</div>
-      <div className="w-full bg-gray-200 rounded-full h-3 mb-2">
+      <div className="w-full bg-stone-200 rounded-full h-3 mb-2">
         <div
-          className="bg-blue-500 h-3 rounded-full"
+          className="bg-emerald-400 h-3 rounded-full"
           style={{ width: `${hobby.progress}%` }}
         ></div>
       </div>
