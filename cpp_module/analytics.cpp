d122a64d76d2eae2cd6da1eac5a66e6c6c95a8ec
Mateus Behrend <mateusbehrend22@gmail.com>
2025-06-08 21:56:08 -0700

    Added basic analytics in c++

--- cpp_module/analytics.cpp
+++ cpp_module/analytics.cpp
@@ -19,3 +19,26 @@ struct HobbyAnalytics {
     int completedHobbies;
 };
 
+HobbyAnalytics calculateHobbyAnalytics(const vector<Hobby>& hobbies) {
+    HobbyAnalytics analytic_results;
+    analytic_results.totalHobbies = hobbies.size();
+
+    if (hobbies.empty()){
+        analytic_results.averageProgress = 0.0;
+        analytic_results.completedHobbies = 0;
+        return analytic_results;
+    }
+
+    double totalProgress = 0;
+    int completedCount = 0;
+    for (const auto& hobby : hobbies) {
+        totalProgress += hobby.progress;
+        if (hobby.progress >= 100) {
+            completedCount++;
+        }
+    }
+    analytic_results.averageProgress = totalProgress / hobbies.size();
+    analytic_results.completedHobbies = completedCount;
+
+    return analytic_results;
+}
