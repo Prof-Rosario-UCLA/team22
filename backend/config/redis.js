44d28d237383ecd34f768a364c96800bc056ea28
Chanyu Kim <chanyukim15@gmail.com>
2025-06-10 00:35:42 -0700

    https and ingress update for /api path

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const redisClient = createClient({
-    // url: process.env.REDIS_URL_DEV,
-    url: process.env.REDIS_URL // DEV
+    //url: process.env.REDIS_URL_DEV,
+    url: process.env.REDIS_URL_PROD // DEV
 });
 
 redisClient.on('error', (e) => console.error(`Error creating redis client: `, e))
